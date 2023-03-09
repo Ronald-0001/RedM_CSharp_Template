367e622c62f4e5a52a37c6406c6ddd1a0f447f2c
Ronald-0001 <106472835+Ronald-0001@users.noreply.github.com>
2023-03-09 17:30:14 +0100

    renamed _object to _event & _cb_event for the example's

--- Nui/Dependent/messages.js
+++ Nui/Dependent/messages.js
@@ -31,9 +31,9 @@ $('document').ready(function () {
 /* Nui examples
 
 // example script > nui call
-Nui.Calls["Test"] = (_object) => { console.log("Test call triggered:", JSON.stringify(_object)); };
+Nui.Calls["Test"] = (_event) => { console.log("Test call triggered:", JSON.stringify(_event)); };
 
 // example nui > scropt call
-Nui.Send("Test", { test: "Test sendt from the nui" }, _object => { console.log("Test cb", JSON.stringify(_object)); });
+Nui.Send("Test", { test: "Test sendt from the nui" }, _cb_event => { console.log("Test cb", JSON.stringify(_cb_event)); });
 
 */
--- Nui/app.js
+++ Nui/app.js
@@ -1,7 +1,7 @@
 // main script
 
 // example script > nui call
-Nui.Calls["Test"] = (_object) => { console.log("Test call triggered:", JSON.stringify(_object)); };
+Nui.Calls["Test"] = (_event) => { console.log("Test call triggered:", JSON.stringify(_event)); };
 
 // example nui > scropt call
-Nui.Send("Test", { test: "Test sendt from the nui" }, _object => { console.log("Test cb", JSON.stringify(_object)); });
+Nui.Send("Test", { test: "Test sendt from the nui" }, _cb_event => { console.log("Test cb", JSON.stringify(_cb_event)); });
